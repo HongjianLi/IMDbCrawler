fcfd87bc523d54e70d678c7755592e4d5b528512
Jacky Lee <JackyLeeHongJian@Gmail.com>
2022-10-21 14:35:23 +0800

    Save the last batch.

--- concat.js
+++ concat.js
@@ -14,11 +14,11 @@ for (let k = 0; k < rows.length; ++k) {
 	const outputTensor = tf.image.resizeBilinear(inputTensor, [width * inputTensor.shape[0] / inputTensor.shape[1], width]); // resizeNearestNeighbor
 	tf.dispose(inputTensor);
 	outputTensors.push(outputTensor);
-	if (outputTensors.length === 20) {
+	if (outputTensors.length === 20 || k + 1 === rows.length) {
 		const outputTensor = tf.concat(outputTensors);
 		console.log(outputTensor.shape);
 		const outputBuffer = await tf.node.encodeJpeg(outputTensor);
-		await fs.writeFile(`images/${year}_${k / 20}.jpg`, outputBuffer);
+		await fs.writeFile(`images/${year}_${Math.floor(k / 20)}.jpg`, outputBuffer);
 		tf.dispose(outputTensors);
 		outputTensors.length = 0;
 	}
