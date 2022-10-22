b9df8c54c3bcc2c8eb833ee0846734fd7e240c01
Jacky Lee <JackyLeeHongJian@Gmail.com>
2022-10-22 21:08:00 +0800

    Skip downloading if the image already exists

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import fs from 'fs/promises';
+import { promises as fs, existsSync } from 'fs';
 import https from 'https';
 import puppeteer from 'puppeteer';
 import ProgressBar from 'progress';
@@ -14,6 +14,8 @@ for (let k = 0; k < rows.length; ++k) {
 	const row = rows[k];
 	const title = row[2];
 	bar.tick({ title });
+	const path = `images/${title}.jpg`;
+	if (existsSync(path)) continue;
 	const page = await browser.newPage();
 	await page.goto(`https://www.imdb.com/title/${title}/`, { waitUntil: 'networkidle0' });
 	const src = await page.evaluate(() => (document.querySelector('img.ipc-image').src ));
@@ -25,6 +27,6 @@ for (let k = 0; k < rows.length; ++k) {
 			res.on('end', () => { resolve(Buffer.concat(data)); });
 		}).on('error', error => { reject(error); });
 	});
-	await fs.writeFile(`images/${title}.jpg`, buffer);
+	await fs.writeFile(path, buffer);
 }
 await browser.close();
