5ca4c44bfc37ae47c91ff13f6ab1cff7c2ed230b
Jacky Lee <JackyLeeHongJian@Gmail.com>
2022-10-22 13:47:20 +0800

    Use the native https client to download image instead of puppeteer whose devtools has a 10Mb limitation on the resource content size

--- download.js
+++ download.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import fs from 'fs/promises';
+import https from 'https';
 import puppeteer from 'puppeteer';
 import ProgressBar from 'progress';
 const year = '2022';
@@ -9,7 +10,6 @@ const browser = await puppeteer.launch({
 	executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
 });
 const bar = new ProgressBar('[:bar] :title :current/:total=:percent :elapseds :etas', { total: rows.length });
-const failed = [];
 for (let k = 0; k < rows.length; ++k) {
 	const row = rows[k];
 	const title = row[2];
@@ -18,17 +18,13 @@ for (let k = 0; k < rows.length; ++k) {
 	await titlePage.goto(`https://www.imdb.com/title/${title}/`, { waitUntil: 'networkidle0' });
 	const src = await titlePage.evaluate(() => (document.querySelector('img.ipc-image').src ));
 	await titlePage.close();
-	const image = `${src.split('_').slice(0, 2).join('_')}_.jpg`;
-	const imagePage = await browser.newPage();
-	const resp = await imagePage.goto(image);
-	await resp.buffer().then(async buffer => {
-		await fs.writeFile(`images/${title}.jpg`, buffer);
-	}).catch(error => { // There's a limitation on the resource content size that devtools keep in memory. It's 10Mb for resource. When the limitation is exceeded, calling resp.buffer() will cause ProtocolError: Protocol error (Network.getResponseBody): Request content was evicted from inspector cache
-		failed.push({ title, image });
-	})
-	await imagePage.close();
+	const buffer = await new Promise((resolve, reject) => {
+		https.get(`${src.split('_').slice(0, 2).join('_')}_.jpg`, res => {
+			const data = [];
+			res.on('data', chunk => { data.push(chunk); });
+			res.on('end', () => { resolve(Buffer.concat(data)); });
+		}).on('error', error => { reject(error); });
+	});
+	await fs.writeFile(`images/${title}.jpg`, buffer);
 }
 await browser.close();
-if (failed.length) {
-	console.log('Failed to download', failed);
-}
