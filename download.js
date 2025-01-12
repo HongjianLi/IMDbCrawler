c0b09144a72c4b352eb232e01ac813fb64409456
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-01-12 11:35:32 +0800

    Change user-agent from Windows to Linux

--- download.js
+++ download.js
@@ -17,7 +17,7 @@ for (let k = 0; k < rows.length; ++k) {
 	const path = `images/${title}.jpg`;
 	if (existsSync(path)) continue;
 	const page = await browser.newPage();
-	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0');
+	await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0');
 	await page.setExtraHTTPHeaders({'accept-language': 'en,en-US;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6'});
 	await page.goto(`https://www.imdb.com/title/${title}/`, { waitUntil: 'networkidle0' });
 	const src = await page.evaluate(() => (document.querySelector('img.ipc-image').src ));
